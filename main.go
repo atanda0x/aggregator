f590d45af40af872bf2586fc2319692f9d05756f
atanda0x <atandanafiu21@gmail.com>
2023-09-20 23:53:49 -0700

    added chi, cros, dotenv

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	godotenv.Load(".env")
+
+	postString := os.Getenv("PORT")
+	if postString == "" {
+		log.Fatal("port is not found in environment")
+	}
+
+	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + postString,
+	}
+
+	log.Printf("Server starting on port %v", postString)
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("port: ", postString)
+}
