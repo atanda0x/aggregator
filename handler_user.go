a59fc3b9ec1ffaff6a999ecd60808e59b38f6bed
atanda0x <atandanafiu21@gmail.com>
2023-09-30 00:53:43 -0700

    added middleware auth

--- handler_user.go
+++ handler_user.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/atanda0x/chi-boilerplate-server/internal/auth"
 	"github.com/atanda0x/chi-boilerplate-server/internal/database"
 	"github.com/google/uuid"
 )
@@ -38,18 +37,7 @@ func (cfg *apiConfig) handlerUsersCreate(w http.ResponseWriter, r *http.Request)
 	respondWithJSON(w, http.StatusOK, databaseUserToUser(user))
 }
 
-func (cfg *apiConfig) handlerUsersGet(w http.ResponseWriter, r *http.Request) {
-	apiKey, err := auth.GetAPIKey(r.Header)
-	if err != nil {
-		respondWithError(w, http.StatusUnauthorized, "Couldn't find api key")
-		return
-	}
-
-	user, err := cfg.DB.GetUserByAPIKey(r.Context(), apiKey)
-	if err != nil {
-		respondWithError(w, http.StatusNotFound, "Couldn't get user")
-		return
-	}
+func (cfg *apiConfig) handlerUsersGet(w http.ResponseWriter, r *http.Request, user database.User) {
 
 	respondWithJSON(w, http.StatusOK, databaseUserToUser(user))
 }
