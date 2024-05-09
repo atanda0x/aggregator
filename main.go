b453900c3864b518f69cc149acda2a2398a76da4
atanda0x <atandanafiu21@gmail.com>
2024-05-09 15:38:31 -0700

    port loads

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load(".env")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT is not found")
+	}
+
+	fmt.Println("Port:", port)
+}
