8857633fb34972a57a7b3c105b10bdd063f0426c
atanda0x <atandanafiu21@gmail.com>
2024-05-12 10:28:11 -0700

    added feeds query

--- db/schema/003_feeds.sql
+++ db/schema/003_feeds.sql
@@ -2,7 +2,7 @@
 CREATE TABLE feeds (
     id UUID PRIMARY KEY,
     name TEXT NOT NULL,
-    url TEXT NOT NULL,
+    url TEXT UNIQUE NOT NULL,
     user_id UUID REFERENCES users(id) ON DELETE CASCADE,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL
